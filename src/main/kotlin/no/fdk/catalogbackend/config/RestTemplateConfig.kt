02a7d022e5587a1eafaf61234935a3f0af516551
Nils Ove Tendenes <nils.ove@tendenes.com>
2026-09-16 16:10:22 +0200

    fix: set rest template timeouts

--- src/main/kotlin/no/fdk/catalogbackend/config/RestTemplateConfig.kt
+++ src/main/kotlin/no/fdk/catalogbackend/config/RestTemplateConfig.kt
@@ -2,10 +2,17 @@ package no.fdk.catalogbackend.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.client.SimpleClientHttpRequestFactory
 import org.springframework.web.client.RestTemplate
+import java.time.Duration
 
 @Configuration
 class RestTemplateConfig {
     @Bean
-    fun restTemplate(): RestTemplate = RestTemplate()
+    fun restTemplate(): RestTemplate = RestTemplate(
+        SimpleClientHttpRequestFactory().apply {
+            setConnectTimeout(Duration.ofSeconds(5))
+            setReadTimeout(Duration.ofSeconds(10))
+        },
+    )
 }
